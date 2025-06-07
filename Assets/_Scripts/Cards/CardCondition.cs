de232ab5ad7758b3018d04a67e2a4a4a431b8f72
Leeshap98 <127130059+Leeshap98@users.noreply.github.com>
2025-06-07 13:14:27 +0300

    Custom drawer

--- Assets/_Scripts/Cards/CardCondition.cs
+++ Assets/_Scripts/Cards/CardCondition.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public enum CardConditionType
 {
-    
+    None,
+    OpponentPowerGreaterThan,
+    IsFirstCardPlayed,
+    HasCardTag,
+    RankEquals,
+    TurnNumberEven
 }
+
+[System.Serializable]
+public struct CardCondition
+{
+    public CardConditionType conditionType;
+
+    public int intValue;
+    public string stringValue;
+    public CardRank rankValue;
+}
+
+public enum CardRank { Bronze, Silver, Gold, Legendary }
